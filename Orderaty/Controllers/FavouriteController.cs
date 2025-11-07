d6a958f518d310552972f7d3699b148ea161c430
khaledahmed <khaled.ahmed333774@gmail.com>
2025-11-07 19:19:39 +0200

    add new filter by favourite in browse stores

--- Orderaty/Controllers/FavouriteController.cs
+++ Orderaty/Controllers/FavouriteController.cs
@@ -5,7 +5,6 @@ using Orderaty.Models;
 
 namespace Orderaty.Controllers
 {
-    [Authorize(Roles = "Client")]
     public class FavouriteController : Controller
     {
         private readonly AppDbContext db;
@@ -13,33 +12,31 @@ namespace Orderaty.Controllers
         {
             this.db = db;
         }
+
+        [Authorize(Roles = "Client")]
         public bool Make(string sellerId)
         {
-            if(User.Identity != null && User.Identity.IsAuthenticated)
+            var clientId = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name)?.Id;
+            var isSellerExist = db.Sellers.Any(f => f.Id == sellerId);
+            if (clientId != null && isSellerExist)
             {
-                var clientId = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name)?.Id;
-                var isSellerExist = db.Sellers.Any(f => f.Id == sellerId);
-                if (clientId != null && isSellerExist)
+                var data = db.Favourites.Where(f => f.ClientId == clientId && f.SellerId == sellerId)
+                    .FirstOrDefault();
+                if (data != null)
                 {
-                    var data = db.Favourites.Where(f => f.ClientId == clientId && f.SellerId == sellerId)
-                        .FirstOrDefault();
-                    if (data != null) 
-                    {
-                        data.IsFavourite = !data.IsFavourite;
-                        db.SaveChanges();
-                        return data.IsFavourite;
-                    }
-                    var favourite = new Favourite()
-                    {
-                        ClientId = clientId,
-                        SellerId = sellerId,
-                        IsFavourite = true
-                    };
-                    db.Favourites.Add(favourite);
+                    data.IsFavourite = !data.IsFavourite;
                     db.SaveChanges();
-                    return true;
+                    return data.IsFavourite;
                 }
-                
+                var favourite = new Favourite()
+                {
+                    ClientId = clientId,
+                    SellerId = sellerId,
+                    IsFavourite = true
+                };
+                db.Favourites.Add(favourite);
+                db.SaveChanges();
+                return true;
             }
             return false;
         }
