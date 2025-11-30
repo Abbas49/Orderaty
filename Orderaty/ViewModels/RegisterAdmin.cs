c32b452135c5138a5a70052babe1c27f277c519c
Youssef Mostafa Elkade <youssefmostafaelkade@gmail.com>
2025-11-30 16:28:01 +0200

    Add files

---
+++ Orderaty/ViewModels/RegisterAdmin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Orderaty.ViewModels
+{
+    public class RegisterAdmin
+    {
+        [Required] public required string FullName { get; set; }
+        [Required] public required string UserName { get; set; }
+        [Required, EmailAddress] public required string Email { get; set; }
+        public string? Phone { get; set; }
+        [Required, DataType(DataType.Password)] public required string Password { get; set; }
+        [Required, DataType(DataType.Password), Compare("Password")] public required string ConfirmPassword { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
